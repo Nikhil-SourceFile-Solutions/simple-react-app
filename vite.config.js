8d1b9f6e19c6d96a7a755d8de10f6930cf8cff07
Nikhil Sasikumar <nikhil@sourcefilesolutions.com>
2025-06-27 13:33:37 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
+  base: './', // 👈 important for relative paths in production
+  build: {
     outDir: 'dist'
   }
 })
